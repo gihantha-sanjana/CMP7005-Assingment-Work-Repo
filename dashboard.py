26e2e64928d6ba0a05d2df1ebb9781906f269f16
Gihantha Sanjana <72568678+gihantha-sanjana@users.noreply.github.com>
2024-12-19 15:01:34 +0000

    call the training model

---
+++ dashboard.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+
+# Function to install packages from requirements.txt
+def install_requirements():
+    try:
+        # Install packages using subprocess
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+        print("All packages installed successfully.")
+    except Exception as e:
+        print(f"Error installing packages: {e}")
+
+# Call the function to install requirements
+install_requirements()
+
+import pickle
+# Load the pickle file
+with open("/workspaces/CMP7005-Assingment-Work-Repo/DataSet/random_forest_model.pkl","rb") as file:
+    data = pickle.load(file)
+
+# Load the DataSet
+print(data)
